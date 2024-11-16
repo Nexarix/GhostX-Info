9d793a0703ff58aef1382907c01c2671ac522a08
MrGhostX <mrghostx94@gmail.com>
2024-11-16 17:18:36 +0100

    Add files via upload

---
+++ IpAddress/Ipaddress.py
@@ -0,0 +1,39 @@
+import requests
+
+def track_ip(ip_address):
+    api_url = f"http://ip-api.com/json/{ip_address}?fields=status,continentCode,country,countryCode,region,regionName,city,zip,lat,lon,timezone,currency,isp,org,mobile,proxy,hosting,query"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        details = [
+            f"    \033[1;32m Status: {data['status']}",
+            f"    \033[1;32m IP Address: {data['query']}",
+            f"    \033[1;32m Country: {data['country']}",
+            f"    \033[1;32m City: {data['city']}",
+            f"    \033[1;32m Region: {data['region']}",
+            f"    \033[1;32m ISP: {data['isp']}",
+            f"    \033[1;32m Zip Code: {data['zip']}",
+            f"    \033[1;32m Latitude: {data['lat']}",
+            f"    \033[1;32m Longitude: {data['lon']}",
+            f"    \033[1;32m Time Zone: {data['timezone']}",
+            f"    \033[1;32m Country Code: {data['countryCode']}",
+            f"    \033[1;32m Currency: {data['currency']}",
+            f"    \033[1;32m Region Name: {data['regionName']}",
+            f"    \033[1;32m Organisation: {data['org']}",
+            f"    \033[1;32m Continent Code: {data['continentCode']}",
+            f"    \033[1;32m Mobile Number: {data['mobile']}",
+            f"    \033[1;32m Hosting: {data['hosting']}"
+        ]
+
+        for line in details:
+            print(line)
+
+        with open("ip_details.txt", "w") as file:
+            for line in details:
+                file.write(line + "\n")
+
+        print("\nDetails saved to 'ip_details.txt'")
+
+    else:
+        print("Failed to retrieve IP information")
---
+++ MobileNumber/mobilenumber.py
@@ -0,0 +1,26 @@
+import requests
+import time
+
+
+def get_mobile_number_info(phone_number):
+    api_key = 'aFwivIX8hJHIPYF3VPfyFgFtwIdLJsq6'  # Your NumVerify API key
+    url = f"http://apilayer.net/api/validate?access_key={api_key}&number={phone_number}&country_code=&format=1"
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+
+            if data['valid']:
+                # Display number information
+                print(f"     \033[1;32m Phone Number: {data['international_format']}")
+                print(f"     \033[1;32m Country: {data['country_name']}")
+                print(f"     \033[1;32m Location: {data['location']}")
+                print(f"     \033[1;32m Carrier: {data['carrier']}")
+                print(f"     \033[1;32m Line Type: {data['line_type']}")
+            else:
+                print(f"Invalid phone number: {phone_number}")
+        else:
+            print("Error: Unable to fetch data.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
